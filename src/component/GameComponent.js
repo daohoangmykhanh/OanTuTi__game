b3a55ccc36f33244640347d90eea43c73e75285f
DaoHoangMyKhanh <daohoangmykhanh@gmail.com>
2021-08-08 10:05:20 +0700

    action

--- src/component/GameComponent.js
+++ src/component/GameComponent.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import '../App.css';
 import Bao from '../image/bao.png';
-import Keo from '..//image/keo.png';
+import Keo from '../image/keo.png';
 import Bua from '../image/bua.png';
 import Player from '../image/player.png';
 import PlayerComputer from '../image/playerComputer.png'
 import {connect} from 'react-redux';
 
+
 class GameComponent extends Component {
+
+    state = {
+        visibility: 'hidden',
+        imgSrc: '',
+    }
+
+    changeSrc = (cuoc) => {
+        this.setState({
+            imgSrc: `${cuoc}`,
+            visibility: 'visible',
+        })
+    }
+
     render() {
+
+        let {soBanChoi,soBanThang} = this.props.oanTuTiReducer
+
+       
+
         return (
             <div className="background"  style={{backgroundColor: "black"}}>
                 <div className="col-4">
@@ -17,14 +36,37 @@ class GameComponent extends Component {
                         <div className="player__item">
                             <div className = "speech__item">
                                 
-                                <div className = "speech__bubble"></div>
+                                    <img className="src__player" style={{visibility: `${this.state.visibility}`}} src={this.state.imgSrc} alt="" />
+                                <div className = "speech__bubble">
+                                </div>
                             </div>
                                 <img src={Player} alt="hinhminhhoa" />
                             </div>
                             <div>
-                                <button className="button__item"> <img src={Keo} alt="keo.png" /></button>
-                                <button className="button__item"> <img src={Bua} alt="bua.png"/></button>
-                                <button className="button__item"> <img src={Bao} alt="bao.png"/></button>
+                                <button className="button__item" onClick = {() => {
+                                    const action = {
+                                        type: 'DAT CUOC',
+                                        banChon: 'Keo'
+                                    }
+                                    this.props.dispatch(action);
+                                    this.changeSrc(Keo);
+                                }}> <img src={Keo} alt="keo.png" /></button>
+                                <button className="button__item" onClick = {() => {
+                                    const action = {
+                                        type: 'DAT CUOC',
+                                        banChon: 'Bua'
+                                    }
+                                    this.props.dispatch(action);
+                                    this.changeSrc(Bua);
+                                }}> <img src={Bua} alt="bua.png"/></button>
+                                <button className="button__item" onClick = {() => {
+                                    const action = {
+                                        type: 'DAT CUOC',
+                                        banChon: 'Bao'
+                                    }
+                                    this.props.dispatch(action);
+                                    this.changeSrc(Bao);
+                                }}> <img src={Bao} alt="bao.png"/></button>
                             </div>
                         </div>
                     </div>
@@ -34,10 +76,15 @@ class GameComponent extends Component {
                         <div >
                             <h1 className="text__title"> I'm iron man, i love you 3000 !!</h1>
                                 <div className="text__container">
-                                    <h1 className="text__info"> Số bàn thắng:</h1>
-                                    <h1 className="text__info"> Số bàn chơi: </h1>
+                                    <h1 className="text__info"> Số bàn thắng: <span> {soBanThang}</span></h1>
+                                    <h1 className="text__info"> Số bàn chơi: <span> {soBanChoi}</span></h1>
                                 </div>
-                                <button className="playgame"> Play Game</button>
+                                <button className="playgame" onClick={()=> {
+                                    const action = {
+                                        type:'PLAY_GAME'
+                                    }
+                                    this.props.dispatch(action);
+                                }}> Play Game</button>
                             </div>
                     </div>
                 </div>
@@ -47,6 +94,7 @@ class GameComponent extends Component {
                             <div>
                                 <div className="player__computer__item">
                                     <div className = "speech__item">
+                                       
                                         <div className = "speech__bubble"></div>
                                     </div>
                                         <img src={PlayerComputer} alt="hinhminhhoa" />
@@ -63,10 +111,7 @@ class GameComponent extends Component {
 const mapStateToProps = (rootReducer) => {
 
     return{
-        oanTuTiReducer: rootReducer.oanTuTiReducer,
-        soBanChoi: rootReducer.soBanChoi,
-        soBanThang: rootReducer.soBanThang,
-        banChon: rootReducer.banChon
+        oanTuTiReducer: rootReducer.oanTuTiReducer
     }
 }
 
--- src/redux/reducer/oanTuTi.js
+++ src/redux/reducer/oanTuTi.js
@@ -1,11 +1,46 @@
 const stateDefault = {
     soBanChoi:0,
     soBanThang:0,
-    banChon:true,
+    hinhAnh:'',
+    banChon:'',
+    display: 'none',
 }
 
 export const oanTuTiReducer = (state = stateDefault, action) => {
+    console.log(action)
     switch(action.type){
+
+        case 'DAT_CUOC': {
+            state.banChon = action.banChon;
+            return {...state};
+        }
+
+        case 'PLAY_GAME': {
+            let soNgauNhien = Math.floor(Math.random() * 3) + 1;
+            if (soNgauNhien === 1){
+                state.hinhAnh = 'Keo';
+            } else if (soNgauNhien === 2){
+                state.hinhAnh = 'Bao';
+            } else {
+                state.hinhAnh = 'Bua';
+            }
+
+            console.log('state',state.hinhAnh)
+
+            if(state.hinhAnh === 'Keo' && state.banChon === 'Bua'){
+                state.soBanThang += 1;
+            } else if (state.hinhAnh === 'Bao' && state.banChon === 'Keo'){
+                state.soBanThang += 1;
+            } else if (state.hinhAnh === 'Bua' && state.banChon === 'Bao'){
+                state.soBanThang += 1;
+            }
+
+            state.soBanChoi += 1;
+            
+
+            return {...state}
+            
+        }
         default: return state;
     }
 }
